d0c2c8362f1409fcc8460676e1ff9b51c2a4c36c
Stefania Sperani <thedarkcry@hotmail.it>
2024-06-21 12:11:45 +0200

    CRUD finished

--- task-manager/src/index.js
+++ task-manager/src/index.js
@@ -6,92 +6,170 @@ const Task = require('./models/task')
 //creo il server
 const app = express()
 const port = process.env.PORT || 3000
-
 //trasformo in json
 app.use(express.json())
 
 //creo lo user
-app.post('/users', (req, res) => {
+app.post('/users', async (req, res) => {
   const user = new User(req.body)
 
-  user
-    .save()
-    .then(() => {
-      res.status(201).send(user)
-    })
-    .catch((e) => {
-      res.status(400).send(e)
-    })
+  try {
+    await user.save()
+    res.status(201).send(user)
+  } catch (e) {
+    res.status(400).send(e)
+  }
 })
 
 //prendo tutta la lista degli users
-app.get('/users', (req, res) => {
-  User.find({})
-    .then((users) => {
-      res.send(users)
-    })
-    .catch((e) => {
-      res.status(500).send(e)
-    })
+app.get('/users', async (req, res) => {
+  try {
+    const users = await User.find({})
+    res.send(users)
+  } catch (e) {
+    res.status(500).send(e)
+  }
 })
 
 //prendo il singolo utente
-app.get('/users/:id', (req, res) => {
+app.get('/users/:id', async (req, res) => {
   const _id = req.params.id
 
-  User.findById(_id)
-    .then((user) => {
-      if (!user) {
-        return res.status(404).send()
-      }
+  try {
+    const user = await User.findById(_id)
+    if (!user) {
+      returnres.status(400).send()
+    }
+    res.send(user)
+  } catch (e) {
+    res.status(500).send({ error: 'Internal server error' })
+  }
+})
 
-      res.send(user)
+//Aggiorno
+app.patch('/users/:id', async (req, res) => {
+  const _id = req.params.id
+
+  //Se vogliono fare una modifica che non è compresa nell'array allora imposto un messaggio di errore
+  const updates = Object.keys(req.body)
+  const allowesUpdates = ['name', 'email', 'password', 'age']
+  const isValidOperation = updates.every((update) => allowesUpdates.includes(update))
+
+  if (!isValidOperation) {
+    return res.status(400).send({ error: 'Invalid updates!' })
+  }
+
+  try {
+    const user = await User.findByIdAndUpdate(_id, req.body, {
+      new: true,
+      runValidators: true,
     })
-    .catch((e) => {
-      res.status(500).send({ error: 'Internal server error' })
-    })
+    if (!user) {
+      return res.status(404).send()
+    }
+    res.send(user)
+  } catch (e) {
+    res.status(400).send(e)
+  }
+})
+
+//Cancello
+app.delete('/users/:id', async (req, res) => {
+  const _id = req.params.id
+
+  try {
+    const user = await User.findByIdAndDelete(_id)
+
+    if (!user) {
+      return res.status(404).send()
+    }
+
+    res.send(user)
+  } catch {
+    res.status(500).send(e)
+  }
 })
 
 //creo il task
-app.post('/tasks', (req, res) => {
+app.post('/tasks', async (req, res) => {
   const task = new Task(req.body)
 
-  task
-    .save()
-    .then(() => {
-      res.status(201).send(task)
-    })
-    .catch((e) => {
-      res.status(400).send(e)
-    })
+  try {
+    await task.save()
+    res.status(201).send(task)
+  } catch (e) {
+    res.status(400).send(e)
+  }
 })
 
 //recupero l'intera lista dei task
-app.get('/tasks', (req, res) => {
-  Task.find({})
-    .then((tasks) => {
-      res.send(tasks)
-    })
-    .catch((e) => {
-      res.status(500).send(e)
-    })
+app.get('/tasks', async (req, res) => {
+  try {
+    const tasks = await Task.find({})
+    res.send(tasks)
+  } catch (e) {
+    res.status(500).send(e)
+  }
 })
 
 //prendo il singolo task
-app.get('/tasks/:id', (req, res) => {
+app.get('/tasks/:id', async (req, res) => {
   const _id = req.params.id
 
-  Task.findById(_id)
-    .then((task) => {
-      if (!task) {
-        return res.status(404).send()
-      }
+  try {
+    const task = await Task.findById(_id)
+    if (!task) {
+      return res.status(404).send()
+    }
 
-      res.send(task)
+    res.send(task)
+  } catch (e) {
+    res.status(500).send({ error: 'Internal server error' })
+  }
+})
+
+//Aggiorno
+app.patch('/tasks/:id', async (req, res) => {
+  const _id = req.params.id
+
+  //Se vogliono fare una modifica che non è compresa nell'array allora imposto un messaggio di errore
+  const updates = Object.keys(req.body)
+  const allowesUpdates = ['description', 'complete']
+  const isValidOperation = updates.every((update) => allowesUpdates.includes(update))
+
+  if (!isValidOperation) {
+    return res.status(400).send({ error: 'Invalid updates!' })
+  }
+
+  try {
+    const task = await Task.findByIdAndUpdate(_id, req.body, {
+      new: true,
+      runValidators: true,
     })
-    .catch((e) => {
-      res.status(500).send({ error: 'Internal server error' })
-    })
+    if (!task) {
+      return res.status(404).send()
+    }
+    res.send(task)
+  } catch (e) {
+    res.status(400).send(e)
+  }
+})
+
+//Cancello
+app.delete('/tasks/:id', async (req, res) => {
+  const _id = req.params.id
+
+  try {
+    const task = await Task.findByIdAndDelete(_id)
+
+    if (!task) {
+      return res.status(404).send()
+    }
+
+    res.send(task)
+  } catch {
+    res.status(500).send(e)
+  }
 })
 
 //apro il server
